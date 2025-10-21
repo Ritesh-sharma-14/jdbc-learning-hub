7e801f822fd8d29e0f5645336a55cff9971bfe34
Ritesh Sharma <r.sharma1402004@gmail.com>
2025-10-21 23:07:57 +0530

    Create DBConnection class for database access
    Added DBConnection class for MySQL database connection.

--- Mini-project/CRUD-Project/src/com/example/DBConnection.java
+++ Mini-project/CRUD-Project/src/com/example/DBConnection.java
@@ -1,1 +1,13 @@
+package com.example;
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String USER = "root"; // change to your MySQL username
+    private static final String PASSWORD = "root"; // change to your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
 
